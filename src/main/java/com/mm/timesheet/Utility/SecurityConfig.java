4181d0ecb8b84931bad9fc094dc863fa638e92df
ANH.TUANN <Fvl123456@>
2025-09-11 15:58:01 +0700

    Fix bug CORS (1)

---
+++ src/main/java/com/mm/timesheet/Utility/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.mm.timesheet.Utility;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable) // tắt CSRF nếu API public
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource)); // ✅ cấu hình CORS mới
+
+        return http.build();
+    }
+}
+
+
