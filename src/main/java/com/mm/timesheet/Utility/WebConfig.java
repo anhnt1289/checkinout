525ea554dda82ecf80ba7597effcf795b101220f
ANH.TUANN <Fvl123456@>
2025-09-11 12:36:52 +0700

    add code deploy

--- src/main/java/com/mm/timesheet/Utility/WebConfig.java
+++ src/main/java/com/mm/timesheet/Utility/WebConfig.java
@@ -8,10 +8,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Cấu hình đường dẫn muốn cho phép
-                .allowedOrigins("*") // Cho phép yêu cầu từ domain này
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP cho phép
-                .allowedHeaders("*") // Cho phép tất cả các header
-                .allowCredentials(true); // Cho phép gửi cookie
     }
 }
