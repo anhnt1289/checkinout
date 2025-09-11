92e2d3fa805da5d7ea2357d3267f9229cdd43fd5
ANH.TUANN <Fvl123456@>
2025-09-11 11:49:55 +0700

    add cros

---
+++ src/main/java/com/mm/timesheet/Utility/WebConfig.java
@@ -0,0 +1,17 @@
+package com.mm.timesheet.Utility;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Cấu hình đường dẫn muốn cho phép
+                .allowedOrigins("https://anhnt1289.github.io") // Cho phép yêu cầu từ domain này
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Các phương thức HTTP cho phép
+                .allowedHeaders("*") // Cho phép tất cả các header
+                .allowCredentials(true); // Cho phép gửi cookie
+    }
+}
